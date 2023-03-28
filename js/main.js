9677b235a115281947b6b8b5d1684373861b0a7f
Klaudiusz Sobczak <klaudeksobczak@gmail.com>
2023-03-28 18:12:50 +0200

    base JS added

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const arcadeTitle = [ARCADE];
+
+const bars = document.querySelector('.fa-bars');
+const hiddenMenu = document.querySelector('.side-menu');
+const exit = document.querySelector('.fa-x');
+const playBtn = document.getElementById('play');
+const gamesSection = document.getElementById('games');
+const h1Title = document.addEventListener('title');
+
+const toggleActive = () => {
+    hiddenMenu.classList.toggle('active');
+    exit.classList.toggle('active');
+    bars.classList.toggle('active');
+};
+
+bars.addEventListener('click', toggleActive);
+
+exit.addEventListener('click', toggleActive);
+
+playBtn.addEventListener('click', function () {
+    gamesSection.scrollIntoView();
+});
