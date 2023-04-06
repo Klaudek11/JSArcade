9fcf5030b2e5c4390df39a7660cbcfa608050bb4
Klaudiusz Sobczak <klaudeksobczak@gmail.com>
2023-04-06 18:55:05 +0200

    rock paper scissors ended

--- js/rock_paper_scissors.js
+++ js/rock_paper_scissors.js
@@ -13,6 +13,8 @@ const hands = [...document.querySelectorAll('div.game-panel i')];
 
 const btnPlay = document.querySelector('div.play');
 
+const reset = document.querySelector('div.reset');
+
 const rockPaperScissors = {
     rock: 'fa-solid fa-hand-back-fist',
     paper: 'fa-solid fa-hand',
@@ -20,11 +22,11 @@ const rockPaperScissors = {
 };
 
 const gamesResult = {
-    pc: 'result',
-    you: 'result',
+    pc: '',
+    you: '',
 };
 
-//Game
+//Your and PC choice
 function yourChoice() {
     hands.forEach((el) => el.classList.remove('active'));
     this.classList.add('active');
@@ -37,45 +39,62 @@ function pcChoice() {
     gamesResult.pc = Object.keys(rockPaperScissors)[handId];
 }
 
+// Stats
 function result() {
     if (gamesResult.pc === gamesResult.you) {
         scorePc.textContent++;
         scoreYou.textContent++;
-        resultTable.textContent = 'REMIS!'
+        resultTable.textContent = 'REMIS!';
     } else if (
         (gamesResult.pc === 'paper' && gamesResult.you === 'scissors') ||
         (gamesResult.pc === 'rock' && gamesResult.you === 'paper') ||
         (gamesResult.pc === 'scissors' && gamesResult.you === 'rock')
     ) {
         scoreYou.textContent++;
-        resultTable.textContent = 'YOU WIN!'
+        resultTable.textContent = 'YOU WIN!';
     } else if (
         (gamesResult.pc === 'scissors' && gamesResult.you === 'paper') ||
         (gamesResult.pc === 'paper' && gamesResult.you === 'rock') ||
         (gamesResult.pc === 'rock' && gamesResult.you === 'scissors')
     ) {
         scorePc.textContent++;
-        resultTable.textContent = 'PC WIN!'
+        resultTable.textContent = 'PC WIN!';
     }
 }
 
+//Game
 function game() {
-    pcChoice();
-    result();
+    if (gamesResult.you !== '') {
+        pcChoice();
+        result();
+    } else {
+        alert('You have to choose!');
+    }
     hands.forEach((el) => el.classList.remove('active'));
+    gamesResult.you = '';
+}
+
+//Reset
+function resetGame() {
+    scorePc.textContent = 0;
+    scoreYou.textContent = 0;
+    pcHand.classList = 'fa-solid fa-question';
+    resultTable.textContent = 'Play now again!';
 }
 
 //Menu
-
 const toggleActive = () => {
     hiddenMenu.classList.toggle('active');
     exit.classList.toggle('active');
     bars.classList.toggle('active');
+    gamesResult.you = '';
+    gamesResult.pc = '';
 };
 
 //Listeners
 hands.forEach((el) => el.addEventListener('click', yourChoice));
-
 btnPlay.addEventListener('click', game);
+reset.addEventListener('click', resetGame);
+
 bars.addEventListener('click', toggleActive);
 exit.addEventListener('click', toggleActive);
